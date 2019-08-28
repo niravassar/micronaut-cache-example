233402aa029ad57082945e955cad4a8f7dd838b5
niravassar <assarn@objectcomputing.com>
2019-08-28 16:37:41 -0500

    first commit

---
+++ src/test/groovy/micronaut/cache/example/controller/HelloControllerSpec.groovy
@@ -0,0 +1,26 @@
+package micronaut.cache.example.controller
+
+import io.micronaut.http.HttpRequest
+import io.micronaut.http.client.RxHttpClient
+import io.micronaut.http.client.annotation.Client
+import io.micronaut.test.annotation.MicronautTest
+import spock.lang.Specification
+
+import javax.inject.Inject
+
+@MicronautTest
+class HelloControllerSpec extends Specification {
+
+    @Inject
+    @Client("/")
+    RxHttpClient client
+
+    void "test hello world response"() {
+        when:
+        HttpRequest request = HttpRequest.GET('/hello')
+        String rsp = client.toBlocking().retrieve(request)
+
+        then:
+        rsp == "Hello World"
+    }
+}
